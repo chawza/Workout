114357aacc493ade1b45c7e8322b920e178fa51c
Nabeel Kahlil Maulana <nabeelkahlil403@gmail.com>
2026-05-31 11:22:44 +0700

    fix bug on rotate: move out koin init into Application level. not activity level (#9)

--- androidApp/src/main/kotlin/com/nabeelkm/workout/MainActivity.kt
+++ androidApp/src/main/kotlin/com/nabeelkm/workout/MainActivity.kt
@@ -6,19 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.nabeelkm.workout.database.appContext
-import com.nabeelkm.workout.di.initKoin
-import org.koin.android.ext.koin.androidContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        appContext = applicationContext
-
-        initKoin {
-            androidContext(applicationContext)
-        }
 
         setContent {
             App()
